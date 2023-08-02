150963ed09d519ef512f76784648c98f8308cccc
ecations <51368173+ecations@users.noreply.github.com>
2023-08-02 15:23:55 +0300

    replugging lines has become unstable again

--- js/debug.js
+++ js/debug.js
@@ -1,3 +1,29 @@
+
+$(document).ready(function () {
+
+//UNCOMMENT TO GET DOM CHANGES: 
+  /*   theMasterObjectInTheBeginning = _.cloneDeep(gameDialogueMakerProject);
+
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                console.log('A child node has been added or removed.');
+                console.log('Parent node:', mutation.target);
+            }
+            else if (mutation.type === 'attributes') {
+                console.log('The ' + mutation.attributeName + ' attribute was modified on element:', mutation.target);
+                console.log('Element ID:', mutation.target.id);
+                console.log('Element class list:', mutation.target.classList.toString());
+                console.log('Element data attributes:', mutation.target.dataset);
+            }
+        }
+    });
+
+    observer.observe(document, { attributes: true, childList: true, subtree: true }); */
+
+
+});
+
 function diffObjects(obj1, obj2, path = '', visited = new WeakSet()) {
     if (_.isEqual(obj1, obj2) || visited.has(obj1) || visited.has(obj2)) {
         return [];
